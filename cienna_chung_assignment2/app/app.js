c680c582b40b0669228b1b5528e707401ff2efd3
chung93 <78765514+chung93@users.noreply.github.com>
2021-07-22 21:01:30 -0700

    create initial files
    link html accordingly; add basic styling to css

---
+++ cienna_chung_assignment2/app/app.js
@@ -0,0 +1,13 @@
+//All of the below should be done on a single index.html page. create, remove or hide and show objects where needed.
+
+//clicking on the New Budget button should allow users to create a new budget. 
+//Users should be able to give the budget a title, create budget items and delete a budget. The items should add or subtract from a total (total should be created as you add items)
+
+//After a budget has been created the main view should display your budgets Title and total in a card
+
+//clicking on a budget card should open that budget up for further editing.
+
+//Store your budgets in an Array of budget items. `[ { title item1, item2, total } ]`
+
+
+//button to make new budget, which adds to the index. the new budget should become a button for editing. oh boy
